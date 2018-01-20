481954146eb22ab3cb7635670424073f75aecf80
Test-Dev-ops <bipulranjankumar@gmail.com>
2018-01-20 11:37:03 +0530

    Update AppTest.java

--- src/test/java/com/qaagility/controller/AppTest.java
+++ src/test/java/com/qaagility/controller/AppTest.java
@@ -2,7 +2,7 @@ package example;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import java.com.qaagility.controller;
+import java.com.qaagility.controller.*;
  
 public class AppTest {
   Calculator calc = new Calculator();
